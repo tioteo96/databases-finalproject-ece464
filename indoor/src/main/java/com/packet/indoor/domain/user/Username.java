17d88a3e7cc5cd9d9a4ca191caa948f242eb6e78
tioteo96 <ohmytheo@gmail.com>
2021-12-03 17:56:51 -0500

    tmp;

--- indoor/src/main/java/com/packet/indoor/domain/user/Username.java
+++ indoor/src/main/java/com/packet/indoor/domain/user/Username.java
@@ -1,8 +1,23 @@
 package com.packet.indoor.domain.user;
 
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Username {
+    @Column(length = 100, unique = true, nullable = false)
     private String name;
+    @Column(length = 100)
+    private String group;
+
+    @Override
+    public String toString(){
+        String groupName = (StringUtils.isNotEmpty(this.group)) ? this.group : "NoGroup";
+        StringBuilder stringBuilder = new StringBuilder(this.name);
+        stringBuilder.append("@");
+        stringBuilder.append(groupName);
+        return stringBuilder.toString();
+    }
 }
