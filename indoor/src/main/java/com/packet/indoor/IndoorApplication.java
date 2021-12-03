eb8d563897e78c1c3638c0e38e525e8fff4f3a05
tioteo96 <ohmytheo@gmail.com>
2021-12-03 14:46:36 -0500

    .

---
+++ indoor/src/main/java/com/packet/indoor/IndoorApplication.java
@@ -0,0 +1,15 @@
+package com.packet.indoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorApplication.class, args);
+		System.out.println("Hello World");
+
+	}
+
+}
